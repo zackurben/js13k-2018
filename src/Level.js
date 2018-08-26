2848fe8338d4c7f832fda070e6442c4f25132a38
Zack Urben <zackurben@gmail.com>
2018-08-25 19:03:40 -0500

    Adding remaining template levels.

--- src/Level.js
+++ src/Level.js
@@ -2,6 +2,12 @@
 
 import Wall from './Wall';
 import one from './levels/1';
+import two from './levels/2';
+import three from './levels/3';
+import four from './levels/4';
+import five from './levels/5';
+import six from './levels/6';
+import seven from './levels/7';
 
 /**
  * Convert the input data into walls.
@@ -16,7 +22,13 @@ const toWalls = input => input.map(w => new Wall(w));
 
 // The level map.
 const levels = {
-  1: toWalls(one)
+  1: toWalls(one),
+  2: toWalls(two),
+  3: toWalls(three),
+  4: toWalls(four),
+  5: toWalls(five),
+  6: toWalls(six),
+  7: toWalls(seven)
 };
 
 export default class Level {
