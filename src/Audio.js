9f072fdc805935e08ee695aa1324a21a30f13a9d
Zack Urben <zackurben@gmail.com>
2018-09-11 23:05:09 -0500

    Adding more crude sound support.

--- src/Audio.js
+++ src/Audio.js
@@ -3,6 +3,9 @@
 export default class Audio {
   constructor() {
     this.context = new (window.AudioContext || window.webkitAudioContext)();
+    this.volume = this.context.createGain();
+    this.volume.connect(this.context.destination);
+    this.volume.gain.value = 0.2;
   }
 
   pickup() {
@@ -10,13 +13,21 @@ export default class Audio {
     this.play(550, 0.05, 0.05);
   }
 
+  load() {
+    this.play(293.66, 0.2, 0, 'square');
+    this.play(349.23, 0.2, 0.2, 'square');
+    this.play(440.0, 0.2, 0.4, 'square');
+  }
+
+  music() {}
+
   play(freq = 440, len = 0.1, off = 0, type = 'sine') {
     let osc = this.context.createOscillator();
     osc.frequency.value = freq;
     osc.type = type;
 
     let now = this.context.currentTime;
-    osc.connect(this.context.destination);
+    osc.connect(this.volume);
     osc.start(now + off);
     osc.stop(now + off + len);
   }
